1fa346536054f4c02f02bd1b55b1146d17307284
Alex McDaniel <amcdaniel186@gmail.com>
2018-11-25 21:47:26 -0500

    update2

--- script.js
+++ script.js
@@ -17,12 +17,41 @@ int main(void)
         counter += 1; 
         // same as counter ++ and counter = counter + 1;
     }
-  }
-  initials[3]
-  printf("%s\n", initals);
+//   }
+//   initials[3]
+//   printf("%s\n", initals);
 
 
-// sequence is also known as Array.
+// // sequence is also known as Array.
 
-computer can only look at one thing at a time.
+// computer can only look at one thing at a time.
+
+// to find one number.
+// integers back to back.
+
+// sorting data can help with algorithms much more efficiently.
+
+// left to right search called: linear search
+
+// exam books at front of class. compare two at a time.
+
+// comparing two at a time results in an extreme value ending up at end of Array.
+
+// selection sort - comparing lowest value to current value.
+
+// bubble sort - repeat until no swaps. pairwise (neighbor) elements.
+
+n-1 refers to end of the list.
+
+running time
+
+n-1 pairs if each pair of elements is counted.
+
+(n-1) + (n-2) + ... + 1
+n(n-1)/2
+
+upper bound running time, O notation
+order of n^2 steps 
+
+lower bound algorithm running time omega notation
 
