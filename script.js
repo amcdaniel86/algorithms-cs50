979bb8d1e1b4766768e89d825ccf16fb1e7b777b
Alex McDaniel <amcdaniel186@gmail.com>
2018-11-25 20:56:57 -0500

    update1

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 // Algorithms notes
 
+to use RAM, memory, you have to allocate it.
+
+4 gb ram, 4 billion bytes of ram.
+
+int main(void)
+{
+  string s = get_string("Name: ");
+  char initials[4];
+  int counter = 0;
+  for (int i = 0; i < strlen(name); i++)
+  {
+    if (isupper[i]))
+    {
+        initials[i] = name[i];
+        counter += 1; 
+        // same as counter ++ and counter = counter + 1;
+    }
+  }
+  initials[3]
+  printf("%s\n", initals);
+
+
+// sequence is also known as Array.
+
+computer can only look at one thing at a time.
+
